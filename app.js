32c14541da9a997f9ee583e9276de3fe1ecddb7d
serj321 <77907685+serj321@users.noreply.github.com>
2021-11-15 18:45:07 -0500

    updated code
    added some comments to explain code functionality.
    Fixed css bug.

--- app.js
+++ app.js
@@ -13,33 +13,41 @@ const User = require('./models/user');
 const session = require('express-session');
 const {isLoggedIn, isUserAdmin} = require('./middleware');
 
+//connects to local database
 mongoose.connect('mongodb://localhost:27017/seneca-connect-contacts', {
     useNewURLParser: true,
     useUnifiedTopology: true
 });
-
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error"));
 db.once("open", () =>{
     console.log("Database connected");
 });
 
+//sets up ejs as the view engine and sets up the view directory to the current directory
+//then views
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+//configurating a session
 const sessionConfig = {
-    secret: 'thisshouldbeabettersecret!',
+    secret: 'notagoodsecret',
     resave: false,
     saveUninitialized: true,
     cookie: {
         httpOnly: true, 
+        // sets up the session to expire a week from now and sets a max
+        // age of a week. This formula will take the current date and add on 
+        // a week in milliseconds to it.
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        // this formula sets the maxAge as a week in milliseconds.
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
 }
-
 app.use(session(sessionConfig));
+
+//sets up passport which is used for authentication
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
@@ -51,11 +59,15 @@ app.use(methodOverride('_method'));
 
 app.use(express.static(__dirname) );
 
+//assigns the user in the session to the currentUser local variable 
+//everytime a page is entered.
 app.use((req, res, next) => {
     res.locals.currentUser = req.user;
     next();
 })
 
+
+//takes the user to the home page
 app.get('/', (req, res) => {
     res.render('home')
 })
@@ -102,16 +114,19 @@ app.get('/contacts', isLoggedIn, async (req, res) => {
     res.render('contacts/index', { people } );
 })
 
+//provides the new contact page
 app.get('/contacts/new', isLoggedIn, isUserAdmin, (req,res) => {
     res.render('contacts/new', {titles, programs});
 })
 
-
-app.get('/contacts/:id/edit', isUserAdmin, async(req, res) => {
-    const contact = await Contact.findById(req.params.id)
+//provides the edit page with all th eusers current information
+app.get('/contacts/:id/edit', isLoggedIn, isUserAdmin, async (req, res) => {
+    const contact = await Contact.findById(req.params.id);
     res.render('contacts/edit', {contact, titles, programs});
 })
 
+//uses parameters provided by the user to dynamically build a mongoose query to return the results
+//that the user is looking for
 app.get('/contacts/filtered', isLoggedIn, async(req, res) =>{
     console.log(req.query);
     var title, name, program;
@@ -133,27 +148,31 @@ app.get('/contacts/filtered', isLoggedIn, async(req, res) =>{
         query["name"] = name;
     }
     const people = await Contact.find(query);
-    res.render('contacts/index', { people })
+    res.render('contacts/index', { people });
 })
 
+//creates a new contact and adds it to the database
 app.post('/contacts', isUserAdmin, async(req, res) =>{
     const contact = new Contact(req.body.contact);
     await contact.save();
-    res.redirect(`/contacts`)
+    res.redirect(`/contacts`);
 })
 
+//updates contact with new given information
 app.put('/contacts/:id', isUserAdmin, async(req, res) => {
     const { id } = req.params;
     await Contact.findByIdAndUpdate(id, { ...req.body.contact })
     res.redirect('/contacts');
 })
 
+//deletes a contact
 app.delete('/contacts/:id', isUserAdmin, async(req, res) => {
     const { id } = req.params;
     await Contact.findByIdAndDelete(id);
     res.redirect('/contacts');
 })
 
+//server will listen on port 3000
 app.listen(3000, ()=>{
     console.log('Serving on port 3000')
 })
--- middleware.js
+++ middleware.js
@@ -1,3 +1,5 @@
+//checks if someone is logged in mby checking the .isAuthenticated() method that is automatically
+//added to the req parameter by passport
 module.exports.isLoggedIn = (req, res, next) =>{
     if(!req.isAuthenticated()){
         return res.redirect('/login');
@@ -5,6 +7,7 @@ module.exports.isLoggedIn = (req, res, next) =>{
     next();
 }
 
+//checks if the current user in teh session is an admin by using the .isAdmin boolean variable
 module.exports.isUserAdmin = (req, res, next) =>{
     if(!req.user.isAdmin){
         return res.redirect('../contacts');
